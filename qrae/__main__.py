876902ff36abf4dadc354db342d050064c082cec
FaridSuleymanov <52218006+FaridSuleymanov@users.noreply.github.com>
2026-05-01 23:21:32 +0200

    Delete qrae directory

--- qrae/__main__.py
+++
@@ -1,6 +0,0 @@
-"""Run QRAE with `python -m qrae`."""
-
-from .cli import main
-
-if __name__ == "__main__":
-    main()
--- qrae/protocols/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Protocol and channel assessment modules."""
-
-from .channel import assess_unprotected_channel
-from .tls import scan_tls_endpoint
-
-__all__ = ["assess_unprotected_channel", "scan_tls_endpoint"]
